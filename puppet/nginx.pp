0dbe3614a1802d769ccacf140cb244a140168ad7
Brett Wright <br1212937@cidronpay.com>
2018-04-11 21:15:03 +1000

    Add a loop

--- puppet/nginx.pp
+++ puppet/nginx.pp
@@ -8,26 +8,40 @@ service { "nginx":
     enable => true
 }
 
-file { "/etc/nginx/nginx.conf":
+
+$files = [ 'nginx.conf', 'server.key', 'server.crt' ]
+
+
+file { "/etc/nginx/${files}":
     require => Package["nginx"],
     ensure => present,
-    source => "file:///tmp/cloudformation/puppet/files/nginx.conf",
+    source => "file:///tmp/cloudformation/puppet/files/${files}",
     notify => Service["nginx"]
 }
 
-file { "/etc/nginx/server.key":
-    require => Package["nginx"],
-    ensure => present,
-    source => "file:///tmp/cloudformation/puppet/files/server.key",
-    notify => Service["nginx"]
-}
 
-file { "/etc/nginx/server.crt":
-    require => Package["nginx"],
-    ensure => present,
-    source => "file:///tmp/cloudformation/puppet/files/server.crt",
-    notify => Service["nginx"]
-}
+#file { "/etc/nginx/nginx.conf":
+#    require => Package["nginx"],
+#    ensure => present,
+#    source => "file:///tmp/cloudformation/puppet/files/nginx.conf",
+#    notify => Service["nginx"]
+#}
+
+#file { "/etc/nginx/server.key":
+#    require => Package["nginx"],
+#    ensure => present,
+#    source => "file:///tmp/cloudformation/puppet/files/server.key",
+#    notify => Service["nginx"]
+#}
+
+#file { "/etc/nginx/server.crt":
+#    require => Package["nginx"],
+#    ensure => present,
+#    source => "file:///tmp/cloudformation/puppet/files/server.crt",
+#    notify => Service["nginx"]
+#}
+
+
 
 
 file{ "/usr/share/nginx/html/index.html":
